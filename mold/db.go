5f969ddc77880cd6c168354642d1abfbeb955d65
streambinder <posta@davidepucci.it>
2021-08-19 17:15:37 +0200

    mold: db: add chunking selections

--- mold/db.go
+++ mold/db.go
@@ -1,7 +1,9 @@
 package mold
 
+import "sort"
+
 var (
-	history []*Event
+	history []*Event = []*Event{}
 )
 
 func Persist(e *Event) error {
@@ -9,6 +11,13 @@ func Persist(e *Event) error {
 	return nil
 }
 
-func Select() ([]*Event, error) {
+func Select(chunk int) ([]*Event, error) {
+	sort.Slice(history, func(i, j int) bool {
+		return history[i].Timestamp.After(history[j].Timestamp)
+	})
+
+	if chunk != -1 && chunk < len(history) {
+		return history[:chunk], nil
+	}
 	return history, nil
 }
