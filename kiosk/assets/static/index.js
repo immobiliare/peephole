4f620fdd124e390634980457bed4902d6590f7b0
streambinder <posta@davidepucci.it>
2021-08-19 16:10:14 +0200

    kiosk: kick some styling in

---
+++ kiosk/assets/static/index.js
@@ -0,0 +1,6 @@
+// (new EventSource("/stream")).addEventListener("event", function (e) {
+//     document.body.innerHTML += e.data + "</br>";
+// });
+function dialog() {
+    console.log('dialog clicked');
+}
