20867b0182072d43aa5b5e4f9493b4323e9d22f0
streambinder <posta@davidepucci.it>
2021-08-20 18:21:47 +0200

    kiosk: add proper per-event dialog

--- kiosk/assets/static/index.js
+++ kiosk/assets/static/index.js
@@ -1,8 +1,25 @@
 var es = null;
 
 function dialog(jid) {
-    console.log('dialog placeholder for jid ' + jid);
+    fetch(`/events/${jid}`)
+        .then((response) => {
+            return response.json();
+        }).then((json) => {
+            let body = document.getElementsByTagName('body')[0];
+            body.innerHTML = `
+            <div class="event-dialog">
+                <a class="dismiss" onclick="dismiss()">close</a>
+                <pre>` + JSON.stringify(JSON.parse(json.RawData), null, 2) + `</pre>
+            </div> ` + body.innerHTML;
+        });
 }
+
+function dismiss() {
+    let dialog = document.getElementsByClassName('event-dialog');
+    if (dialog.length > 0) {
+        dialog[0].remove();
+    }
+};
 
 function setLoader(value) {
     if (value == null) {
@@ -20,9 +37,9 @@ function addEvent(e) {
         <div class="event">
             <span class="minion">${e.Minion}</span>
             <span class="function">${e.Function}</span>
-            <a class="show" onclick="dialog(${e.Jid})">show</a>
+            <a class="show" onclick="dialog('${e.Jid}')">show</a>
         </div>
-    </li>` + document.getElementsByClassName('event-list')[0].innerHTML;
+    </li> ` + document.getElementsByClassName('event-list')[0].innerHTML;
 }
 
 window.onload = async function () {
@@ -39,8 +56,12 @@ window.onload = async function () {
             }
         });
     es = new EventSource("/stream");
-    es.onopen = function () { }
-    es.onerror = function () { }
+    es.onopen = function () {
+        console.log('stream opened');
+    }
+    es.onerror = function () {
+        console.log('stream closed');
+    }
     es.addEventListener('event', function (e) {
         addEvent(JSON.parse(e.data))
     }, false);
@@ -49,5 +70,11 @@ window.onload = async function () {
 window.onbeforeunload = async function () {
     if (es != null) {
         es.close();
+    }
+}
+
+document.onkeydown = async function (e) {
+    if (e.which == 27) {
+        dismiss();
     }
 }
