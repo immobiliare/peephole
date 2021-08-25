32000d41a70a93d6401023c365c65e603079a461
streambinder <posta@davidepucci.it>
2021-08-25 12:43:40 +0200

    config: bring some unit tests in

--- config/wrapper.go
+++ config/wrapper.go
@@ -1,9 +1,13 @@
 package config
 
 import (
+	"io/ioutil"
+	"os"
+
 	_kiosk "github.com/streambinder/peephole/kiosk"
 	_mold "github.com/streambinder/peephole/mold"
 	_spy "github.com/streambinder/peephole/spy"
+	"gopkg.in/yaml.v2"
 )
 
 // Wrapper represents the abstraction of the parsed
@@ -12,4 +16,47 @@ type Wrapper struct {
 	Spy   []*_spy.Config `yaml:"spy"`
 	Kiosk *_kiosk.Config `yaml:"kiosk"`
 	Mold  *_mold.Config  `yaml:"mold"`
+}
+
+// Parse generates a new Config instance
+// starting from a configuration file path
+func Parse(path string) (*Wrapper, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return nil, err
+	}
+
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return parseYaml(content)
+}
+
+func parseYaml(content []byte) (*Wrapper, error) {
+	config := Wrapper{
+		Spy:   []*_spy.Config{},
+		Mold:  new(_mold.Config),
+		Kiosk: new(_kiosk.Config),
+	}
+	if err := yaml.Unmarshal(content, &config); err != nil {
+		return nil, err
+	}
+
+	return process(&config)
+}
+
+func process(cfg *Wrapper) (*Wrapper, error) {
+	for _, e := range cfg.Spy {
+		if err := e.Validate(); err != nil {
+			return nil, err
+		}
+	}
+	if err := cfg.Kiosk.Validate(); err != nil {
+		return nil, err
+	}
+	if err := cfg.Mold.Validate(); err != nil {
+		return nil, err
+	}
+	return cfg, nil
 }
---
+++ config/wrapper_test.go
@@ -0,0 +1,70 @@
+package config
+
+import (
+	"testing"
+
+	_kiosk "github.com/streambinder/peephole/kiosk"
+	_mold "github.com/streambinder/peephole/mold"
+	_spy "github.com/streambinder/peephole/spy"
+)
+
+var (
+	spy = _spy.Config{
+		API:    "http://localhost:8080",
+		User:   "user",
+		Pass:   "pass",
+		Client: "pam",
+	}
+	kiosk = _kiosk.Config{
+		Bind:      ":8080",
+		BasicAuth: map[string]string{},
+	}
+	mold = _mold.Config{
+		Spool:     "/var/spool/peephole",
+		Retention: "7d",
+	}
+)
+
+func TestParseValid(t *testing.T) {
+	cfg, err := parseYaml([]byte(`
+spy:
+  - api: ` + spy.API + `
+    user: ` + spy.User + `
+    pass: ` + spy.Pass + `
+    client: ` + spy.Client + `
+kiosk:
+  bind: ` + kiosk.Bind + `
+mold:
+  spool: ` + mold.Spool + `
+  retention: ` + mold.Retention,
+	))
+
+	if err != nil {
+		t.Errorf("Supplied config is not supposed to return an error")
+	}
+
+	cond := len(cfg.Spy) == 1
+	cond = cond && cfg.Spy[0].API == spy.API
+	cond = cond && cfg.Spy[0].User == spy.User
+	cond = cond && cfg.Spy[0].Pass == spy.Pass
+	cond = cond && cfg.Spy[0].Client == spy.Client
+	cond = cond && cfg.Kiosk.Bind == kiosk.Bind
+	cond = cond && len(cfg.Kiosk.BasicAuth) == 0
+	cond = cond && cfg.Mold.Spool == mold.Spool
+	cond = cond && cfg.Mold.Retention == mold.Retention
+	if !cond {
+		t.Errorf("Parsed config is not corresponding to supplied data")
+	}
+}
+
+func TestParseEmpty(t *testing.T) {
+	if _, err := parseYaml([]byte("")); err == nil {
+		t.Errorf("Empty config is supposed to return an error")
+	}
+}
+
+func TestParseUnexisting(t *testing.T) {
+	if _, err := Parse("/tmp/not-existing-file"); err == nil {
+		t.Errorf("Non-existing config file is supposed to not return a valid config instance")
+	}
+}
