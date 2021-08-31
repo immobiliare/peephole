1f714eceafd8b878e0f03b8144ba2c2625d69341
streambinder <posta@davidepucci.it>
2021-08-31 12:23:34 +0200

    mold: outline: revert logic bringing everything by default

--- mold/outline.go
+++ mold/outline.go
@@ -1,13 +1,7 @@
 package mold
 
-func (e *Event) Outline() Event {
-	return Event{
-		Master:    e.Master,
-		Minion:    e.Minion,
-		Tag:       e.Tag,
-		Jid:       e.Jid,
-		Function:  e.Function,
-		Timestamp: e.Timestamp,
-		Success:   e.Success,
-	}
+func (e *Event) Outline() (outline Event) {
+	outline = *e
+	outline.RawData = ""
+	return
 }
