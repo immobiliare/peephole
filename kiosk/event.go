45c71910431b5cd0638b78888b84ee88ddbfb2a1
streambinder <posta@davidepucci.it>
2021-09-01 16:25:30 +0200

    kiosk: event: return 404 if entry is nil without err

--- kiosk/event.go
+++ kiosk/event.go
@@ -11,6 +11,8 @@ func (k *Kiosk) eventHandler(c *gin.Context) {
 	if e, err := k.mold.Read(c.Param("jid")); err != nil {
 		logrus.WithError(err).WithField("jid", c.Param("jid")).Errorln("Unable to query event")
 		c.Status(http.StatusNotFound)
+	} else if e == nil {
+		c.Status(http.StatusNotFound)
 	} else {
 		c.Writer.Header().Set("Content-Type", "application/json")
 		c.Next()
