e35712e67e6c681db9d267838929ed85b05e2b96
streambinder <posta@davidepucci.it>
2021-10-11 15:35:19 +0200

    mold: write: pass event time as record timestamp

--- mold/write.go
+++ mold/write.go
@@ -18,7 +18,7 @@ func (db *Mold) Write(e *_event.Event) error {
 			if err != nil {
 				return err
 			} else {
-				return tx.Put(bucket, []byte(e.Jid), bytes, r)
+				return tx.PutWithTimestamp(bucket, []byte(e.Jid), bytes, r, uint64(e.Timestamp.Unix()))
 			}
 		})
 }
