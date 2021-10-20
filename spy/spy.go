47aa52e11c53aa81994957be9fa5c18b55808a2b
streambinder <posta@davidepucci.it>
2021-10-20 13:29:21 +0200

    spy: fix reconnecting to lost Salt master

--- spy/spy.go
+++ spy/spy.go
@@ -12,9 +12,16 @@ import (
 )
 
 type Spy struct {
-	endpoints map[string]string
+	endpoints map[string]*endpoint
 	EventChan chan *_event.Event
 	db        *_mold.Mold
+}
+
+type endpoint struct {
+	user     string
+	password string
+	client   string
+	token    string
 }
 
 func init() {
@@ -25,18 +32,20 @@ func init() {
 
 func Init(db *_mold.Mold, endpoints []*Config) (*Spy, error) {
 	var spy = &Spy{
-		make(map[string]string),
+		make(map[string]*endpoint),
 		make(chan *_event.Event),
 		db,
 	}
 	for _, e := range endpoints {
-		r, err := _salt.Login(e.API, e.User, e.Pass, e.Client)
+		endpoint := &endpoint{e.User, e.Pass, e.Client, ""}
+		token, err := login(e.API, endpoint)
 		if err != nil {
 			return nil, err
 		}
 
-		spy.endpoints[e.API] = r.Return[0].Token
-		logrus.WithField("token", r.Return[0].Token).Debugln("Token received")
+		endpoint.token = token
+		spy.endpoints[e.API] = endpoint
+		logrus.WithField("token", token).Debugln("Token received")
 	}
 	return spy, nil
 }
@@ -51,7 +60,11 @@ func (s *Spy) Endpoints() (e []string) {
 func (s *Spy) Watch() error {
 	peephole := make(chan *_salt.EventsResponse, len(s.endpoints))
 	for k, v := range s.endpoints {
-		go s.spy(k, v, peephole)
+		go func(api string, endpoint *endpoint) {
+			if err := s.spy(api, endpoint, peephole); err != nil {
+				logrus.WithError(err).WithField("endpoint", endpoint).Fatalln("Unable to spy")
+			}
+		}(k, v)
 	}
 
 	for {
@@ -83,12 +96,32 @@ func (s *Spy) Watch() error {
 	}
 }
 
-func (s *Spy) spy(endpoint, token string, peephole chan *_salt.EventsResponse) error {
+func (s *Spy) spy(api string, endpoint *endpoint, peephole chan *_salt.EventsResponse) error {
 	for {
-		err := _salt.Events(endpoint, token, peephole)
-		if err != nil {
-			logrus.WithError(err).Warnln("Spying interrupted, retrying")
+		if s.endpoints[api].token == "EXPIRED" {
+			logrus.WithField("endpoint", api).Println("Reauthenticating")
+			tok, err := login(api, endpoint)
+			if err != nil {
+				logrus.WithError(err).WithField("endpoint", api).Errorln("Unable to authenticate")
+				continue
+			}
+			s.endpoints[api].token = tok
 		}
-		time.Sleep(1 * time.Minute)
+
+		err := _salt.Events(api, endpoint.token, peephole)
+		if err != nil {
+			logrus.WithError(err).WithField("endpoint", api).Warnln("Spying interrupted, retrying")
+			s.endpoints[api].token = "EXPIRED"
+		}
+
+		time.Sleep(5 * time.Second)
 	}
+}
+
+func login(api string, endpoint *endpoint) (string, error) {
+	r, err := _salt.Login(api, endpoint.user, endpoint.password, endpoint.client)
+	if err != nil {
+		return "", err
+	}
+	return r.Return[0].Token, nil
 }
--- spy/spy_test.go
+++ spy/spy_test.go
@@ -6,13 +6,13 @@ import (
 
 func TestEndpoints(t *testing.T) {
 	var (
-		s = Spy{endpoints: map[string]string{"1": "1"}}
+		s = Spy{endpoints: map[string]*endpoint{"api": {"user", "pass", "client", "token"}}}
 		e = s.Endpoints()
 	)
 	if len(e) != 1 {
 		t.Errorf("Wrong endpoints size")
 	}
-	if e[0] != "1" {
+	if e[0] != "api" {
 		t.Errorf("Wrong endpoint value")
 	}
 }
